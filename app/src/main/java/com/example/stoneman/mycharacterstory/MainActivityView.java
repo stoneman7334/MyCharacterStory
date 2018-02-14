37165fb35a75934a89d5844ac5781c26ec3b62e8
5151002 <5151002@st.hsc.ac.jp>
2018-02-14 21:38:41 +0900

    MainActivityView作成

---
+++ app/src/main/java/com/example/stoneman/mycharacterstory/MainActivityView.java
@@ -0,0 +1,14 @@
+package com.example.stoneman.mycharacterstory;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Created by Stoneman on 2018/02/14.
+ */
+
+public class MainActivityView extends View {
+    public MainActivityView(Context context) {
+        super(context);
+    }
+}
